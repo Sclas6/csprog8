5d3760f4fe1870c82e61d07af3ff348343f72e32
Ayato-Suzuki <suzuki.ayato.19@shizuoka.ac.jp>
2019-12-13 11:29:31 +0900

    実装しました

--- MapGameController.java
+++ MapGameController.java
@@ -59,6 +59,10 @@ public class MapGameController implements Initializable {
             downButtonAction();
         }else if (key == KeyCode.RIGHT){
             rightButtonAction();
+        }else if (key == KeyCode.LEFT){
+            leftButtonAction();
+        }else if (key == KeyCode.UP){
+            upButtonAction();
         }
     }
 
@@ -83,6 +87,25 @@ public class MapGameController implements Initializable {
         mapPrint(chara, mapData);
     }
     public void rightButtonAction(ActionEvent event) {
-        rightButtonAction();
+       rightButtonAction();
+
+    }
+    public void leftButtonAction(){ 　　 /**左に進む*/
+        chara.setCharaDir(MoveChara.TYPE_LEFT);
+        chara.move( -1, 0);
+        mapPrint(chara, mapData);
+    }
+    public void leftButtonAction(ActionEvent event) {
+        leftButtonAction();
+    }g
+
+    public void upButtonAction(){　　　/**上に進む*/
+        outputAction("UP");
+        chara.setCharaDir(MoveChara.TYPE_UP);
+        chara.move( 0, -1);
+        mapPrint(chara, mapData);
+    }
+    public void upButtonAction(ActionEvent event) {
+        upButtonAction();
     }
 }
